0390872549a49c3bf7822f904a3f1886a7b7389c
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-04-10 20:56:51 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 868 ms, Memory: 236732 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Main {
+
+	public static void main(String[] args)throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(bf.readLine());
+		
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+			bw.write((a+b) + "\n");
+		} 
+		bw.flush();
+        bw.close();
+	} 
+}
