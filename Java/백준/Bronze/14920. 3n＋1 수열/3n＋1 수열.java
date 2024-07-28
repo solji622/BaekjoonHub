d90e92b9db0ba0a901ecb66cba8873eb9a897052
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-07-28 21:33:34 +0900

    [Bronze III] Title: 3n+1 수열, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+
+        while (true) {
+            if (n == 1) {
+                cnt++;
+                System.out.println(cnt);
+                break;
+            }
+
+            if (n % 2 == 0) {
+                n = n / 2;
+                cnt++;
+            } else {
+                n = 3 * n + 1;
+                cnt++;
+            }
+        }
+
+    }
+}
