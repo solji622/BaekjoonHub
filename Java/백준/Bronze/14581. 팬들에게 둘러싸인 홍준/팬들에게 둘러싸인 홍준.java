c1926e7ffa7b5f59a7c7cbe3cb83384a0f13ccc3
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-07-30 20:48:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 196 ms, Memory: 18456 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.nextLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.print(":fan:"+":"+id+":"+":fan:\n");
+        System.out.println(":fan::fan::fan:");
+        
+    }
+}
