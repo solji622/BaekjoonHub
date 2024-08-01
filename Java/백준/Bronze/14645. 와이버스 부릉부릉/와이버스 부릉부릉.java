a3eb7443f1f39c9a64a1ce9df33a9702c7985360
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-08-01 15:43:22 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 212 ms, Memory: 17624 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if (i == N) {
+                System.out.println("비와이");
+                break;
+            }
+
+        }
+    }
+}
