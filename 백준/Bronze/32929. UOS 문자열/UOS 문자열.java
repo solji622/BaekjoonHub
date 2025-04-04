1f9c3b195c8f355e2ba0e2e6546a12d6a1e3c312
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-04-04 14:16:49 +0900

    [Bronze V] Title: UOS 문자열, Time: 172 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 3 == 1) {
+            System.out.println("U");
+        } else if(n % 3 == 2) {
+            System.out.println("O");
+        } else if (n % 3 == 0) {
+            System.out.println("S");
+        }
+    }
+}
