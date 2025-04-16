3f603a6fe1ccbd253434bf24acfd723f9d2ceffd
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-04-16 10:03:05 +0900

    [Bronze IV] Title: 영수증, Time: 184 ms, Memory: 17888 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int X = scan.nextInt();
-		int N = scan.nextInt();
-		
-		int sum = 0;
-		for (int i=0; i<N; i++) {
-			int a = scan.nextInt();
-			int b = scan.nextInt();
-			sum += (a * b);
-		}
-		
-		if (X == sum) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
-		}
-	}
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        int sum = 0;
 
+        for (int i = 0; i < n; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            for (int j = 0; j < b; j++) {
+                sum += a;
+            }
+        }
+
+        if (sum == x) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }
